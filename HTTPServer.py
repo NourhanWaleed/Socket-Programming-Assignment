857f7bcaaca3008805f25375728dae171af001e3
Esraa-Mahmoud-Mohamed <104640363+Esraa-Mahmoud-Mohamed@users.noreply.github.com>
2022-05-08 16:51:51 +0200

    Update HTTPServer.py

--- HTTPServer.py
+++ HTTPServer.py
@@ -12,7 +12,6 @@ POST = "POST"
 status_200 = "status 200"
 status_404 = "status 404"
 
-
 def retreive_page(url):
     f = urllib.request.urlopen(url)
     page = f.read().decode("UTF-8")
@@ -20,73 +19,53 @@ def retreive_page(url):
     return page
 
 
-def receive_from_client(conn, addr):
+def receive_from_client(conn,addr):
     print(f"New client {address} connected.")
 
     connected = True
     outputdata = "failed"
     while connected:
         message = conn.recv(1024).decode()      # buffer is 1MB
-        if message.split()[0] == 'GET':
+        if message.split()[0] == 'GET' :
             # TODO: pick out name of requested file
             try:
                 filename = message.split()[1]
                 print(filename)
-                # outputdata = retreive_page(filename)
+                #outputdata = retreive_page(filename)
                 f = open(filename[0:])
-                outputdata = f.read()
-
-                # send the Http header which is formatted as 200 OK
-                conn.send(bytes("HTTP/1.0 200 OK\r\n", "UTF-8"))
-                # Send the content of the requested file to the client
-                # for i in range(0, len(outputdata)+10):
-                # conn.send(outputdata[i].encode())
-                # print(outputdata[i].encode())
-                # conn.send("\r\n".encode())
+                outputdata = f.read()       #opens requiered file and reads its content
                 f.close()
-                #send the Http header which is formatted as 200 OK 
-                conn.send(bytes("HTTP/1.0 200 OK\r\n","UTF-8"))
-                #Send the content of the requested file to the client
-                #for i in range(0, len(outputdata)+10):
-                    #conn.send(outputdata[i].encode())
-                    #print(outputdata[i].encode())
-                #conn.send("\r\n".encode())
-
+                 
+                conn.send(bytes("HTTP/1.0 200 OK\r\n","UTF-8"))     #send the Http header which is formatted as 200 OK
+               
                 
-                # connected = False
             except: # if file not found
-                conn.send(bytes("HTTP/1.0 404 Not Found\r\n","UTF-8"))
-               # connected = False
+               conn.send(bytes("HTTP/1.0 404 Not Found\r\n","UTF-8"))     #send the Http header which is formatted as 404 Not Found
+               
                
         elif message.split()[0] == POST:
             try:
-
-                outputdata = message.split('/')[1] 
-                f = open('newfile.txt', 'w')
-                f.write(outputdata)        
-
                 outputdata = message.split()[1] 
-                newpath = shutil.copy(outputdata,"E:\esraa\Computer Networks\prog.Ass\ServerFolder")
+                newpath = shutil.copy(outputdata,"E:\esraa\Computer Networks\prog.Ass\ServerFolder")    #copy the required file into ServerFolder (simulating sending it to server)
                 conn.send(bytes("HTTP/1.0 200 OK\r\n","UTF-8"))     
-
                 # TODO: wait for uploaded file from client
             except:
-                conn.send(bytes("HTTP/1.0 404 Not Found\r\n", "UTF-8"))
+                conn.send(bytes("HTTP/1.0 404 Not Found\r\n","UTF-8"))
         # TODO: handle html files, txt files and png files
         else:
-            outputdata = "unknown message"
+            outputdata = "unknown message"      #for any not supported methods
         if not message:
             break
 
         print(f"[{addr}] {message}")
-        conn.send(bytes(outputdata, "UTF-8"))
+        conn.send(bytes(outputdata,"UTF-8"))     #Send the content of the requested file to the client
         conn.send("\r\n".encode())
     conn.close()
 
 
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:          #just a smol question is this equivilant to while true?
-        s.bind(address)    #this should be before the while true loop
+        s.bind(address)
         s.listen()
         print(f"[LISTENING] Server is listening on {Host}:{port}")
         while True:
@@ -95,9 +74,5 @@ def main():
             thread.start()
             print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")     # what is threading.activecount?
 
-
 if __name__ == "__main__":
     main()
-
-
-
