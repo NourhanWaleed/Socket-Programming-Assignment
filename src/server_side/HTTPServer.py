e7670b8305fd165268986ec5a468e7a9c94cb1dd
Fatema Moharam <89610248+moharamfatema@users.noreply.github.com>
2022-05-11 00:40:00 +0200

    fixed grt response format

--- src/server_side/HTTPServer.py
+++ src/server_side/HTTPServer.py
@@ -1,8 +1,9 @@
 import socket
 import threading
-import urllib
+from urllib import request
 import os.path
 import shutil
+from struct import pack, unpack
 
 PORT = 65432 
 
@@ -11,10 +12,11 @@ SAVE_PATH = "files/output"
 RECV_BUFF = 1024
 GET_RECV_BUFF = 2048
 
-HOST_IDX = 2
-PORT_IDX = 3
 REQUEST_IDX = 0
 FILE_NAME_IDX = 1
+HTTP_VERSION_IDX = 2
+HOST_IDX = 4
+PORT_IDX = 5
 
 GET = "GET"
 POST = "POST"
@@ -22,11 +24,12 @@ STATUS_OK = "200 OK" # TODO:use
 STATUS_NOT_FOUND = "404 Not Found"
 
 LOCAL_HOST = "127.0.0.1"
+DEF_PORT = 80
 
 ADDRESS = (LOCAL_HOST, PORT)
 
 def retreive_page(url):
-    f = urllib.request.urlopen(url)
+    f = request.urlopen(url)
     page = f.read().decode("UTF-8")
     f.close()
     return page
@@ -36,38 +39,54 @@ def receive_from_client(conn,addr):
     print(f"New client {ADDRESS} connected.")
 
     connected = True
-    outputdata = "failed"
+    response = ""
     while connected:
         message = conn.recv(RECV_BUFF).decode()      # TODO: recv bytes w struct and unpack, buffer is 1MB 
         if not message:
             break
 
         msg_tokens = message.split()
-        if msg_tokens[REQUEST_IDX] == GET :
+        # unpacking message contents
+        request = msg_tokens[0]
+        filename = msg_tokens[1].split("/",1)[1]
+        try:
+            _ = msg_tokens.index("HTTP/1.1")
+            version = "HTTP/1.1"
+        except (ValueError):
+            version = "HTTP/1.0"
+
+        try:
+            i = msg_tokens.index("Host:")
+            host = msg_tokens[i+1]
+            try:
+                port = host.split(":")[1]
+                host = host.split(":")[0]
+            except IndexError:
+                port = DEF_PORT
+        except (ValueError):
+            host = LOCAL_HOST
+
+        if request == GET :
             # TODO: pick out name of requested file
             try:
-                filename = msg_tokens[FILE_NAME_IDX]
                 print(filename)
-                #outputdata = retreive_page(filename)
-                f = open(filename[0:])
-                outputdata = f.read()       #opens requiered file and reads its content
-                f.close()
-                 
-                conn.sendall(bytes("HTTP/1.0 200 OK\r\n","UTF-8"))     #TODO:send the Http header which is formatted as 200 OK
-               
-                
+                # outputdata = retreive_page(filename)
+                with open(filename) as f:
+                    outputdata = f.read()       #opens requiered file and reads its content
+                                 
+                response = f"{version} 200 OK\r\n"
+                response += outputdata
             except(FileNotFoundError):
-               conn.sendall(bytes("HTTP/1.0 404 Not Found\r\n","UTF-8"))     #TODO:send the Http header which is formatted as 404 Not Found
-               
-               
-        elif msg_tokens[REQUEST_IDX] == POST:
+               conn.sendall(bytes(f"{version} 404 Not Found\r\n","UTF-8"))     #TODO:send the Http header which is formatted as 404 Not Found
+
+        elif request == POST:
             try:
                 outputdata = msg_tokens[FILE_NAME_IDX] 
                 newpath = shutil.copy(outputdata,SAVE_PATH)    #TODO: clean this up, copy the required file into ServerFolder (simulating sending it to server)
                 conn.sendall(bytes("HTTP/1.0 200 OK\r\n","UTF-8"))     
                 # TODO: wait for uploaded file from client
             except(FileNotFoundError):
-                conn.sendall(bytes("HTTP/1.0 404 Not Found\r\n","UTF-8"))
+                conn.sendall(b"HTTP/1.0 404 Not Found\r\n")
                 break
         # TODO: handle html files, txt files and png files
         else:
@@ -75,7 +94,8 @@ def receive_from_client(conn,addr):
 
 
         print(f"[{addr}] {message}")
-        conn.sendall(bytes(outputdata,"UTF-8"))     # Send the content of the requested file to the client
+        if response != "":     # Send the content of the requested file to the client
+            conn.sendall(bytes(response,"UTF-8")) 
         conn.sendall("\r\n".encode())
     conn.close()
 
