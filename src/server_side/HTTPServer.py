d921be2b3489a972487e681f0bae8710113e9dd7
Fatema Moharam <89610248+moharamfatema@users.noreply.github.com>
2022-05-11 14:24:05 +0200

    sends all file types

--- src/server_side/HTTPServer.py
+++ src/server_side/HTTPServer.py
@@ -67,36 +67,36 @@ def receive_from_client(conn,addr):
             host = LOCAL_HOST
 
         if request == GET :
-            # TODO: pick out name of requested file
             try:
                 print(filename)
-                # outputdata = retreive_page(filename)
-                with open(filename) as f:
+                
+                with open(filename,"rb") as f:
                     outputdata = f.read()       #opens requiered file and reads its content
                                  
-                response = f"{version} 200 OK\r\n"
+                response = bytes(f"{version} 200 OK\r\n","UTF-8")
                 response += outputdata
             except(FileNotFoundError):
-               conn.sendall(bytes(f"{version} 404 Not Found\r\n","UTF-8"))     #TODO:send the Http header which is formatted as 404 Not Found
-
+               conn.sendall(bytes(f"{version} 404 Not Found\r\n","UTF-8"))    
+        # TODO: post
         elif request == POST:
             try:
-                outputdata = msg_tokens[FILE_NAME_IDX] 
+                outdata = msg_tokens[FILE_NAME_IDX] 
                 newpath = shutil.copy(outputdata,SAVE_PATH)    #TODO: clean this up, copy the required file into ServerFolder (simulating sending it to server)
                 conn.sendall(bytes("HTTP/1.0 200 OK\r\n","UTF-8"))     
                 # TODO: wait for uploaded file from client
             except(FileNotFoundError):
                 conn.sendall(b"HTTP/1.0 404 Not Found\r\n")
                 break
-        # TODO: handle html files, txt files and png files
+        
         else:
             outputdata = "unknown message"      #for any not supported methods
 
 
         print(f"[{addr}] {message}")
         if response != "":     # Send the content of the requested file to the client
-            conn.sendall(bytes(response,"UTF-8")) 
+            conn.sendall(response) 
         conn.sendall("\r\n".encode())
+        conn.sendall(b"exitinggggg")
     conn.close()
 
 
